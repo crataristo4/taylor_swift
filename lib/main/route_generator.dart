ab372d161c869eabe0fbccf7a16052709702de3a
Aristocrat <crataristo4@gmail.com>
2021-08-06 18:31:35 +0000

    added route_generator.dart

---
+++ lib/main/route_generator.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:taylor_swift/ui/auth/registration_page.dart';
+import 'package:taylor_swift/ui/auth/verification_page.dart';
+import 'package:taylor_swift/ui/onboarding/onboarding_screen.dart';
+
+class RouteGenerator {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    final args = settings.arguments;
+
+    switch (settings.name) {
+      //shows when user newly installs the application
+      case OnboardingScreen.routeName:
+        return MaterialPageRoute(builder: (_) => OnboardingScreen());
+
+      //screen to register new users / login
+      case RegistrationPage.routeName:
+        return MaterialPageRoute(builder: (_) => RegistrationPage());
+
+      //verify users phone number
+      case VerificationPage.routeName:
+        final data = settings.arguments as String;
+        return MaterialPageRoute(
+            builder: (_) => VerificationPage(
+                  phoneNumber: data,
+                ));
+
+      default:
+        return _errorRoute();
+    }
+  }
+
+  //error page ..
+  static Route<dynamic> _errorRoute() {
+    return MaterialPageRoute(builder: (_) {
+      return Scaffold(
+        body: Center(
+          child: Text("Page not Found"),
+        ),
+      );
+    });
+  }
+}
