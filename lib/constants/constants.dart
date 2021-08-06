7e653dc1dd2f09e43003a0e9013f8d6b756cd2ed
Aristocrat <crataristo4@gmail.com>
2021-08-06 21:57:38 +0000

    added onboarding_screen.dart

---
+++ lib/constants/constants.dart
@@ -0,0 +1,196 @@
+import 'package:flutter/material.dart';
+
+const appTitle = 'Taylor Swift';
+const welcomeToApp = 'Welcome to Taylor Swift';
+const kGhanaCedi = 'GH¢';
+
+const colorizeColors = [
+  Colors.purple,
+  Colors.blue,
+  Colors.yellow,
+  Colors.red,
+];
+
+const colorizeTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontFamily: 'Horizon',
+);
+
+//---------------------------------------------------------------------------------------------------------------
+const help = 'Help';
+const name = 'Name';
+const home = 'Home';
+const price = 'Price';
+const continuE = 'Continue';
+const fewSteps = 'Just a few steps to get you started';
+const manageWorks = 'Manage all your work here';
+const trackProgress = 'Track your work progress from schedules';
+const getNotified = 'Get notified when a work scheduled is due';
+
+const account = 'My Account';
+const orders = 'My Orders';
+const successful = 'Successful';
+const userNameUpdated = 'User name updated';
+const confirmed = 'Confirmed';
+const approve = 'Approve';
+const viewAll = 'View All';
+const seeAll = 'See All';
+const allPastries = "All Pastries";
+const buyNow = 'Buy now';
+const addItem = 'Add Item';
+
+const tapImageToAdd = 'tap to add image';
+const mustSelectImage = "Must select an image";
+const done = 'Done';
+
+const categoryRequired = 'Category required';
+const selectAType = 'Select a type';
+const specialOffer = 'Special Offers';
+const viewedYourProfile = 'Viewed your profile';
+const goodMorning = 'Good Morning';
+const goodAfternoon = 'Good Afternoon';
+const goodEvening = 'Good Evening';
+const goodNight = 'Good Night';
+const fullNameRequired = 'full name required';
+const enterYourName = "Enter your name";
+const realNameDesc = "real names makes you easily identified";
+const profileCreatedSuccessfully = "Profile created successfully";
+const email = 'Email';
+const emailUx = 'Email us';
+const call = 'Call';
+const chatUx = 'Chat Us';
+const callUx = 'Call Us';
+const OK = 'OK!';
+const anErrorOccurred = 'An error occurred';
+const msg = 'Message';
+const report = 'Report';
+const cantAuthenticate = 'Cant Authenticate you, Try Again Later';
+const plsTryAgain = 'Please wait as you have used limited number request';
+const reportDes = 'Report a user or any suspicious action';
+const share = 'Share this app';
+const shareDes = 'Share app to a friend';
+const contactUs = 'Contact Us';
+const quickContact = 'QUICK CONTACT';
+const contactUsDes = 'Having troubles? Let us know';
+const search = 'Search';
+const submitNow = 'Submit Now';
+const containsBlockedMsg =
+    'We have blocked all requests from this device due to unusual activity. Try again later.';
+
+const DistanceBetween = 'Distance Between';
+const DistanceDes =
+    'This app uses your location to find the distance between you and an Artisan';
+const photoLibrary = 'Photo Library';
+const camera = 'Camera';
+const viewLocation = 'View user\'s location';
+const setDeliveryFee = 'Set Delivery Fee';
+const abtToSetDeliveryFee =
+    'You are about to set the delivery fee for this order';
+const setPaymentStatus = 'Set Payment Status';
+const youAreAbtTpSetPaymentStatus = 'Are you sure you want to proceed ?';
+const chooseImage = 'Choose Image';
+const changeImage = 'Choose Photo';
+const couldNotLaunch = "Could not launch";
+const please = 'Please';
+const finish = 'Finish';
+const updateProfile = 'Update Profile';
+const updateItem = 'Update Item';
+const mustSelectPicture = "Must select a picture";
+const nothingToUpdate = "Nothing to update";
+const received = 'Received ';
+const viewProfile = 'View profile';
+const save = 'Save';
+const sent = "Sent ";
+const profile = "Profile ";
+const status = "Status";
+const pleaseWait = 'Please wait...';
+const completingProfile = 'Completing your profile';
+const updatingProfilePicture = 'Updating profile picture...';
+const deletingOrder = 'Deleting order...';
+const followUs = "Follow us";
+const reviews = "Reviews ";
+const writeAReview = "Write a review";
+const aboutUs = "About Gelic Hub";
+const updatingName = 'Updating name...';
+const updated = 'Updated';
+const customerCare = 'Customer care';
+const register = 'Register';
+
+const logout = 'Log Out';
+const logoutDes = 'You will be Logged out from this account';
+const yesLogMeOut = 'Yes! Logout';
+const noCancel = 'No! cancel';
+const verifyNumber = 'Verify number';
+const confirmOTP = 'Confirm code sent';
+const completeProfile = "Complete profile";
+const accountSetUp = "Account Set up";
+const enterCode = 'Enter code';
+const enterVerificationCode = 'Enter the verification code sent to your SMS';
+const cancel = 'Cancel';
+const delete = 'Delete';
+const deleteOrder = 'Delete Order';
+const deleteOrderDes = "This order will be deleted.Do you want to proceed?";
+const edit = 'Edit';
+const yesSet = 'Yes set fee';
+const yes = 'Yes';
+const rate = 'Rate';
+const ratingUser = "rating user";
+const rated = 'Rated';
+const rateNow = 'Rate Now';
+const enterPhoneToLogin = "Enter your phone number to Log in";
+const confirmNumber = 'Confirm number';
+const sendCodeTo = 'Send verification code to ';
+const invalidPhone = 'Invalid phone number';
+const code = 'Code';
+const category = 'Category';
+const setQuantity = "Set quantity";
+const quantityOrdered = "Quantity Ordered";
+const toTAL = "Total";
+const deliveryFee = "Delivery fee";
+const SubTotal = "Subtotal";
+const enterItemDescription = 'Enter the description';
+const description = 'Description';
+const resend = 'Resend code';
+const tryAgainSometime = "Try again in sometime";
+const invalidOTP = "Invalid OTP";
+const labelSkip = 'Skip >>';
+const labelNext = 'Next';
+const labelGetStarted = 'Get started';
+const sendingCode = 'Please wait sending code...';
+const loggingYouOut = 'logging you out ...';
+const unableToConnect = 'Unable to connect';
+const uploading = 'Uploading...';
+const plsScheduleDate = 'Please schedule a day ahead for your order';
+const sixteenDp = 16.0;
+const eightDp = 8.0;
+const eighteenDp = 18.0;
+const fourDp = 4.0;
+const twentyFourDp = 24.0;
+const twentyDp = 20.0;
+const thirtyDp = 30.0;
+const thirtyTwoDp = 32.0;
+const thirtySixDp = 36.0;
+const fortyDp = 40.0;
+const fortyEightDp = 48.0;
+const fiftyDp = 50.0;
+const sixtyDp = 60.0;
+const sixtyFiveDp = 65.0;
+const seventyDp = 70.0;
+const eightyDp = 80.0;
+const tenDp = 10.0;
+const fifteenDp = 15.0;
+const sixDp = 6.0;
+const twelveDp = 12.0;
+const fourteenDp = 14.0;
+const twoHundredDp = 200.0;
+const twoTwentyDp = 220.0;
+const twoFiftyDp = 250.0;
+const oneFiftyDp = 150.0;
+const oneTwentyDp = 120.0;
+const oneEightyDp = 180.0;
+const hundredDp = 100.0;
+const oneThirtyDp = 130.0;
+const oneSixtyDp = 160.0;
+const threeHundredDp = 300.0;
+const threeFiftyDp = 350.0;
+const fourHundredDp = 400.0;
--- lib/ui/onboarding/onboarding_screen.dart
+++ lib/ui/onboarding/onboarding_screen.dart
@@ -1,6 +1,12 @@
-import 'package:firebase_auth/firebase_auth.dart';
+import 'dart:async';
+
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'package:taylor_swift/constants/constants.dart';
+import 'package:taylor_swift/service/admob_service.dart';
+import 'package:taylor_swift/ui/auth/config_page.dart';
+import 'package:taylor_swift/ui/auth/registration_page.dart';
 
 class OnboardingScreen extends StatefulWidget {
   static const routeName = '/onboarding';
@@ -11,21 +17,132 @@ class OnboardingScreen extends StatefulWidget {
   _OnboardingScreenState createState() => _OnboardingScreenState();
 }
 
-class _OnboardingScreenState extends State<OnboardingScreen> {
-  bool? isLoggedIn;
+class _OnboardingScreenState extends State<OnboardingScreen>
+    with SingleTickerProviderStateMixin {
+  bool? isShowing;
+  double? _scale;
+
+  AdmobService admobService = AdmobService();
 
   @override
   void initState() {
-    var user = Provider.of<User?>(context, listen: false);
-    isLoggedIn = user != null;
+    isShowing = false;
+
+    admobService.createInterstitialAd();
     super.initState();
+  }
+
+  _OnboardingScreenState() {
+    Timer.periodic(Duration(seconds: 20), (timer) {
+      admobService.showInterstitialAd();
+      setState(() {
+        isShowing = true;
+      });
+      timer.cancel();
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    print(" ?? --- $isLoggedIn");
     return Scaffold(
-      body: Center(child: Text("heyyyy")),
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        toolbarHeight: 0,
+      ),
+      body: CustomScrollView(
+        slivers: [
+          SliverFillRemaining(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    TextLiquidFill(
+                        text: welcomeToApp,
+                        waveColor: Colors.blueAccent,
+                        boxBackgroundColor: Colors.white,
+                        textStyle: TextStyle(
+                          fontSize: thirtyTwoDp,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        boxHeight: twoHundredDp),
+                    SizedBox(
+                      height: thirtyDp,
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      padding: EdgeInsets.all(eightDp),
+                      margin: EdgeInsets.symmetric(horizontal: eightDp),
+                      child: Center(
+                          child: DefaultTextStyle(
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                            fontSize: twentyDp,
+                            fontFamily: 'Agne',
+                            color: Colors.black),
+                        child: AnimatedTextKit(
+                          pause: Duration(seconds: 4),
+                          totalRepeatCount: 1,
+                          animatedTexts: [
+                            TypewriterAnimatedText(
+                              fewSteps,
+                            ),
+                            TypewriterAnimatedText(manageWorks),
+                            TypewriterAnimatedText(trackProgress),
+                            TypewriterAnimatedText(getNotified),
+                          ],
+                          onTap: () {
+                            Navigator.of(context)
+                                .pushNamed(RegistrationPage.routeName);
+                          },
+                        ),
+                      )),
+                    ),
+                  ],
+                ),
+                buildNextButton()
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
+
+  buildNextButton() {
+    return isShowing!
+        ? Container(
+            margin: EdgeInsets.only(bottom: thirtyDp),
+            child: GestureDetector(
+              onTap: pushToConfigPage,
+              child: SizedBox(
+                height: fortyEightDp,
+                width: MediaQuery.of(context).size.width,
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Theme.of(context).primaryColor,
+                      borderRadius: BorderRadius.circular(eightDp)),
+                  margin: EdgeInsets.symmetric(horizontal: tenDp),
+                  child: Center(
+                      child: Text(
+                    continuE,
+                    style: TextStyle(fontSize: fourteenDp, color: Colors.white),
+                  )),
+                ),
+              ),
+            ),
+          )
+        : Container();
+  }
+
+  pushToConfigPage() {
+    Navigator.of(context)
+        .pushNamedAndRemoveUntil(ConfigurationPage.routeName, (route) => false);
+  }
+/*  void _tapUp(TapUpDetails details) {
+    _controller!.reverse();
+  }*/
+
 }
