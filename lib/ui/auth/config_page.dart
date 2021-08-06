f5b8b8c0c135de5ded5de1609893963f3a717cca
Aristocrat <crataristo4@gmail.com>
2021-08-06 18:53:05 +0000

    added config_page.dart

---
+++ lib/ui/auth/config_page.dart
@@ -0,0 +1,43 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:taylor_swift/ui/auth/registration_page.dart';
+import 'package:taylor_swift/ui/home/home.dart';
+
+class ConfigurationPage extends StatefulWidget {
+  static const routeName = '/';
+
+  const ConfigurationPage({Key? key}) : super(key: key);
+
+  @override
+  _ConfigurationPageState createState() => _ConfigurationPageState();
+}
+
+class _ConfigurationPageState extends State<ConfigurationPage> {
+  bool? isLoggedIn;
+
+  @override
+  void initState() {
+    var user = Provider.of<User?>(context, listen: false);
+    isLoggedIn = user != null;
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print(" ?? --- $isLoggedIn");
+    return WillPopScope(
+      onWillPop: () async => true,
+      child: Container(
+        child: SafeArea(
+            top: false,
+            bottom: false,
+            child: Scaffold(
+                body: isLoggedIn!
+                    ? HomePage() //AdminPage(selectedIndex: 0,) //change to users --todo
+                    : RegistrationPage())),
+      ),
+    );
+  }
+}
