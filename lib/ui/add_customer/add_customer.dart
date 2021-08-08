60659eb80a7ee53f80f6d97d178fbe4118dcde20
Aristocrat <crataristo4@gmail.com>
2021-08-08 11:34:14 +0000

    added addCustomer

---
+++ lib/ui/add_customer/add_customer.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddCustomer extends StatefulWidget {
+  const AddCustomer({Key? key}) : super(key: key);
+
+  @override
+  _AddCustomerState createState() => _AddCustomerState();
+}
+
+class _AddCustomerState extends State<AddCustomer> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
