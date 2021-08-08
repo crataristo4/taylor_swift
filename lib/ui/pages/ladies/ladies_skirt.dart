b4e003cf6d461ccef060900fabf3e9d7b06d45c5
Aristocrat <crataristo4@gmail.com>
2021-08-08 19:21:57 +0000

    added ladies_skirt.dart

--- lib/ui/pages/ladies/ladies_skirt.dart
+++ lib/ui/pages/ladies/ladies_skirt.dart
@@ -1,4 +1,10 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
+import 'package:taylor_swift/constants/constants.dart';
+import 'package:taylor_swift/constants/theme_data.dart';
+import 'package:taylor_swift/ui/widgets/custom_inputs.dart';
+import 'package:taylor_swift/ui/widgets/custom_name.dart';
+import 'package:taylor_swift/ui/widgets/item_rows.dart';
 
 class LadiesSkirt extends StatefulWidget {
   const LadiesSkirt({Key? key}) : super(key: key);
@@ -8,8 +14,101 @@ class LadiesSkirt extends StatefulWidget {
 }
 
 class _LadiesSkirtState extends State<LadiesSkirt> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController waistController = TextEditingController();
+  TextEditingController hipController = TextEditingController();
+  TextEditingController kneeController = TextEditingController();
+  TextEditingController skirtLengthController = TextEditingController();
+
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 0,
+      ),
+      body: Form(
+        key: _formKey,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: tenDp,
+              ),
+              CustomerName(
+                nameController: nameController,
+              ),
+              SizedBox(
+                height: tenDp,
+              ),
+              Center(
+                  child: DefaultTextStyle(
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                    fontSize: twentyDp,
+                    fontFamily: 'Agne',
+                    color: Colors.black),
+                child: AnimatedTextKit(
+                  totalRepeatCount: 1,
+                  animatedTexts: [
+                    TypewriterAnimatedText(cstMeasurement,
+                        textStyle: TextStyle(color: Colors.indigo)),
+                  ],
+                ),
+              )),
+
+              //first row fow measurement
+              ItemRows(
+                widgetA: CustomerInputs(
+                  valueController: waistController,
+                  imageSource: 'assets/images/waist.png',
+                  name: waist,
+                  textColor: CustomColors.c1,
+                ),
+                widgetB: CustomerInputs(
+                  valueController: hipController,
+                  imageSource: 'assets/images/hip.png',
+                  name: hip,
+                  textColor: CustomColors.c4,
+                ),
+              ),
+
+              //second row fow measurement
+              ItemRows(
+                widgetA: CustomerInputs(
+                  valueController: kneeController,
+                  imageSource: 'assets/images/knee.png',
+                  name: waist,
+                  textColor: CustomColors.c1,
+                ),
+                widgetB: CustomerInputs(
+                  valueController: skirtLengthController,
+                  imageSource: 'assets/images/lsk.png',
+                  name: skirtLength,
+                  textColor: CustomColors.c4,
+                ),
+              ),
+
+              ItemRows(
+                  widgetA: Container(),
+                  widgetB: Container(
+                    margin: EdgeInsets.symmetric(
+                        horizontal: twentyDp, vertical: thirtyDp),
+                    child: FloatingActionButton.extended(
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          print('good to goo');
+                        }
+                      },
+                      label: Text(save),
+                    ),
+                  ))
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
