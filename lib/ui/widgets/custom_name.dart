9fd68d978b713767c3c7b1365c15bf390ef5a14a
Aristocrat <crataristo4@gmail.com>
2021-08-11 17:09:57 +0000

    modified

--- lib/ui/widgets/custom_name.dart
+++ lib/ui/widgets/custom_name.dart
@@ -2,39 +2,75 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:taylor_swift/constants/constants.dart';
 
-class CustomerName extends StatelessWidget {
+class CustomNameAndNumber extends StatelessWidget {
   final TextEditingController nameController;
+  final TextEditingController phoneNumberController;
 
-  const CustomerName({Key? key, required this.nameController})
+  const CustomNameAndNumber(
+      {Key? key,
+      required this.nameController,
+      required this.phoneNumberController})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
-      child: TextFormField(
-          maxLength: 30,
-          maxLines: 1,
-          keyboardType: TextInputType.text,
-          maxLengthEnforcement: MaxLengthEnforcement.enforced,
-          controller: nameController,
-          validator: (value) {
-            return value!.trim().length < 3 ? "name required" : null;
-          },
-          decoration: InputDecoration(
-            labelText: 'Customer\'s name',
-            labelStyle: TextStyle(color: Colors.black),
-            helperText: "name of customer",
-            hintStyle: TextStyle(color: Colors.black),
-            fillColor: Color(0xFFF5F5F5),
-            filled: true,
-            contentPadding:
-                EdgeInsets.symmetric(vertical: 0, horizontal: tenDp),
-            enabledBorder: OutlineInputBorder(
-                borderSide: BorderSide(color: Color(0xFFF5F5F5))),
-            border: OutlineInputBorder(
-                borderSide: BorderSide(color: Color(0xFFF5F5F5))),
-          )),
+      child: Column(
+        children: [
+          TextFormField(
+              maxLength: 30,
+              maxLines: 1,
+              keyboardType: TextInputType.text,
+              maxLengthEnforcement: MaxLengthEnforcement.enforced,
+              controller: nameController,
+              validator: (value) {
+                return value!.trim().length < 3 ? "name required" : null;
+              },
+              decoration: InputDecoration(
+                labelText: 'Customer\'s name',
+                labelStyle: TextStyle(color: Colors.black),
+                helperText: "name of customer",
+                hintStyle: TextStyle(color: Colors.black),
+                fillColor: Color(0xFFF5F5F5),
+                filled: true,
+                contentPadding:
+                    EdgeInsets.symmetric(vertical: 0, horizontal: tenDp),
+                enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Color(0xFFF5F5F5))),
+                border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Color(0xFFF5F5F5))),
+              )),
+          SizedBox(
+            height: eightDp,
+          ),
+          TextFormField(
+              maxLength: 12,
+              maxLines: 1,
+              keyboardType: TextInputType.phone,
+              maxLengthEnforcement: MaxLengthEnforcement.enforced,
+              controller: phoneNumberController,
+              validator: (value) {
+                return value!.trim().length < 10
+                    ? "phone number required"
+                    : null;
+              },
+              decoration: InputDecoration(
+                labelText: 'Customer\'s phone',
+                labelStyle: TextStyle(color: Colors.black),
+                helperText: "helps you to call customer",
+                hintStyle: TextStyle(color: Colors.black),
+                fillColor: Color(0xFFF5F5F5),
+                filled: true,
+                contentPadding:
+                    EdgeInsets.symmetric(vertical: 0, horizontal: tenDp),
+                enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Color(0xFFF5F5F5))),
+                border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Color(0xFFF5F5F5))),
+              ))
+        ],
+      ),
     );
   }
 }
