80b198579e86f20c3ddb37b6b86047c4cc2d1f21
Aristocrat <crataristo4@gmail.com>
2021-08-11 18:22:19 +0000

    mens top added

--- lib/ui/pages/mens/mens_top.dart
+++ lib/ui/pages/mens/mens_top.dart
@@ -1,4 +1,11 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
+import 'package:taylor_swift/constants/constants.dart';
+import 'package:taylor_swift/constants/theme_data.dart';
+import 'package:taylor_swift/ui/widgets/custom_dt_payment.dart';
+import 'package:taylor_swift/ui/widgets/custom_inputs.dart';
+import 'package:taylor_swift/ui/widgets/custom_name.dart';
+import 'package:taylor_swift/ui/widgets/item_rows.dart';
 
 class MensTop extends StatefulWidget {
   const MensTop({Key? key}) : super(key: key);
@@ -8,8 +15,131 @@ class MensTop extends StatefulWidget {
 }
 
 class _MensTopState extends State<MensTop> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController phoneNumberController = TextEditingController();
+  TextEditingController chestController = TextEditingController();
+  TextEditingController backController = TextEditingController();
+  TextEditingController sleeveController = TextEditingController();
+  TextEditingController collarController = TextEditingController();
+  TextEditingController cuffController = TextEditingController();
+  TextEditingController lengthController = TextEditingController();
+  TextEditingController aroundArmController = TextEditingController();
+
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 0,
+      ),
+      body: Form(
+        key: _formKey,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: tenDp,
+              ),
+              CustomNameAndNumber(
+                nameController: nameController,
+                phoneNumberController: phoneNumberController,
+              ),
+              SizedBox(
+                height: tenDp,
+              ),
+              Center(
+                  child: DefaultTextStyle(
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                    fontSize: twentyDp,
+                    fontFamily: 'Agne',
+                    color: Colors.black),
+                child: AnimatedTextKit(
+                  totalRepeatCount: 1,
+                  animatedTexts: [
+                    TypewriterAnimatedText(menTopMeasurement,
+                        textStyle: TextStyle(color: Colors.indigo)),
+                  ],
+                ),
+              )),
+
+              //first row fow measurement
+              ItemRows(
+                widgetA: CustomerInputs(
+                  valueController: lengthController,
+                  imageSource: 'assets/images/length.png',
+                  name: length,
+                  textColor: CustomColors.c1,
+                ),
+                widgetB: CustomerInputs(
+                  valueController: backController,
+                  imageSource: 'assets/images/mbk.png',
+                  name: back,
+                  textColor: CustomColors.c4,
+                ),
+              ),
+
+              //second row for measurement
+              ItemRows(
+                widgetA: CustomerInputs(
+                  valueController: sleeveController,
+                  imageSource: 'assets/images/sleeve.png',
+                  name: sleeve,
+                  textColor: CustomColors.c2,
+                ),
+                widgetB: CustomerInputs(
+                  valueController: collarController,
+                  imageSource: 'assets/images/collar.png',
+                  name: collar,
+                  textColor: CustomColors.c6,
+                ),
+              ),
+              //third row .
+              ItemRows(
+                widgetA: CustomerInputs(
+                  valueController: chestController,
+                  imageSource: 'assets/images/chest.png',
+                  name: chest,
+                  textColor: CustomColors.c7,
+                ),
+                widgetB: CustomerInputs(
+                  valueController: aroundArmController,
+                  imageSource: 'assets/images/arm.png',
+                  name: aroundArm,
+                  textColor: CustomColors.c1,
+                ),
+              ),
+              //fourth row .
+              ItemRows(
+                  widgetA: CustomerInputs(
+                    valueController: cuffController,
+                    imageSource: 'assets/images/cuff.png',
+                    name: cuff,
+                    textColor: CustomColors.c4,
+                  ),
+                  widgetB: Container()),
+              //fifth row .
+              CustomDtPmt(),
+
+              Center(
+                child: Container(
+                  margin: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+                  child: FloatingActionButton.extended(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        print('good to goo');
+                      }
+                    },
+                    label: Text(save),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
