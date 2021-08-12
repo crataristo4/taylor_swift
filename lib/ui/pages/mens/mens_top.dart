f5361201256ac38eaf8a6e44b28e4b2d1418627c
Aristocrat <crataristo4@gmail.com>
2021-08-12 17:55:28 +0000

    added logic

--- lib/ui/pages/mens/mens_top.dart
+++ lib/ui/pages/mens/mens_top.dart
@@ -2,13 +2,19 @@ import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:taylor_swift/constants/constants.dart';
 import 'package:taylor_swift/constants/theme_data.dart';
+import 'package:taylor_swift/enum/enums.dart';
+import 'package:taylor_swift/model/dress.dart';
+import 'package:taylor_swift/provider/dress_provider.dart';
+import 'package:taylor_swift/ui/widgets/actions.dart';
 import 'package:taylor_swift/ui/widgets/custom_dt_payment.dart';
 import 'package:taylor_swift/ui/widgets/custom_inputs.dart';
 import 'package:taylor_swift/ui/widgets/custom_name.dart';
 import 'package:taylor_swift/ui/widgets/item_rows.dart';
 
 class MensTop extends StatefulWidget {
-  const MensTop({Key? key}) : super(key: key);
+  final month;
+
+  const MensTop({Key? key, this.month}) : super(key: key);
 
   @override
   _MensTopState createState() => _MensTopState();
@@ -24,8 +30,11 @@ class _MensTopState extends State<MensTop> {
   TextEditingController cuffController = TextEditingController();
   TextEditingController lengthController = TextEditingController();
   TextEditingController aroundArmController = TextEditingController();
-
+  TextEditingController dtController = TextEditingController();
+  TextEditingController initialPaymentController = TextEditingController();
+  TextEditingController serviceChargeController = TextEditingController();
   final _formKey = GlobalKey<FormState>();
+  DressProvider _dressProvider = DressProvider();
 
   @override
   Widget build(BuildContext context) {
@@ -121,16 +130,42 @@ class _MensTopState extends State<MensTop> {
                     textColor: CustomColors.c4,
                   ),
                   widgetB: Container()),
-              //fifth row .
-              CustomDtPmt(),
+              CustomDtPmt(
+                dateTimeController: dtController,
+                paymentController: initialPaymentController,
+                serviceChargeController: serviceChargeController,
+              ),
 
               Center(
                 child: Container(
                   margin: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
                   child: FloatingActionButton.extended(
                     onPressed: () {
-                      if (_formKey.currentState!.validate()) {
-                        print('good to goo');
+                      var status = Dress().checkPaymentStatus(
+                          int.parse(serviceChargeController.text),
+                          int.parse(initialPaymentController.text));
+
+                      if (status.contains(error)) {
+                        ShowAction().showSnackbar(context);
+                      } else {
+                        _dressProvider.setMensTopData(
+                            nameController.text,
+                            phoneNumberController.text,
+                            lengthController.text,
+                            backController.text,
+                            sleeveController.text,
+                            collarController.text,
+                            chestController.text,
+                            aroundArmController.text,
+                            cuffController.text,
+                            widget.month,
+                            int.parse(serviceChargeController.text),
+                            int.parse(initialPaymentController.text),
+                            dtController.text,
+                            status);
+
+                        _dressProvider.createNewDress(
+                            context, DressType.MENS_TOP);
                       }
                     },
                     label: Text(save),
