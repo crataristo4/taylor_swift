21fe394a9b71fe8349a5280b9cff8ddf64fc2cf0
Aristocrat <crataristo4@gmail.com>
2021-08-12 20:46:33 +0000

    modified

--- lib/ui/pages/ladies/ladies_top.dart
+++ lib/ui/pages/ladies/ladies_top.dart
@@ -2,13 +2,19 @@ import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:taylor_swift/constants/constants.dart';
 import 'package:taylor_swift/constants/theme_data.dart';
+import 'package:taylor_swift/enum/enums.dart';
+import 'package:taylor_swift/model/dress.dart';
+import 'package:taylor_swift/provider/dress_provider.dart';
+import 'package:taylor_swift/ui/widgets/actions.dart';
 import 'package:taylor_swift/ui/widgets/custom_dt_payment.dart';
 import 'package:taylor_swift/ui/widgets/custom_inputs.dart';
 import 'package:taylor_swift/ui/widgets/custom_name.dart';
 import 'package:taylor_swift/ui/widgets/item_rows.dart';
 
 class LadiesTop extends StatefulWidget {
-  const LadiesTop({Key? key}) : super(key: key);
+  final month;
+
+  const LadiesTop({Key? key, required this.month}) : super(key: key);
 
   @override
   _LadiesTopState createState() => _LadiesTopState();
@@ -27,7 +33,10 @@ class _LadiesTopState extends State<LadiesTop> {
   TextEditingController topLengthController = TextEditingController();
   TextEditingController sleeveLengthController = TextEditingController();
   TextEditingController aroundArmController = TextEditingController();
-
+  TextEditingController dtController = TextEditingController();
+  TextEditingController initialPaymentController = TextEditingController();
+  TextEditingController serviceChargeController = TextEditingController();
+  DressProvider _dressProvider = DressProvider();
   final _formKey = GlobalKey<FormState>();
 
   @override
@@ -72,10 +81,10 @@ class _LadiesTopState extends State<LadiesTop> {
               //first row fow measurement
               ItemRows(
                 widgetA: CustomerInputs(
-                  valueController: waistController,
-                  imageSource: 'assets/images/waist.png',
-                  name: waist,
-                  textColor: CustomColors.c1,
+                  valueController: shoulderController,
+                  imageSource: 'assets/images/shoda.png',
+                  name: shoulder,
+                  textColor: CustomColors.c6,
                 ),
                 widgetB: CustomerInputs(
                   valueController: bustController,
@@ -86,21 +95,6 @@ class _LadiesTopState extends State<LadiesTop> {
               ),
 
               //second row for measurement
-              ItemRows(
-                widgetA: CustomerInputs(
-                  valueController: hipController,
-                  imageSource: 'assets/images/hip.png',
-                  name: hip,
-                  textColor: CustomColors.c2,
-                ),
-                widgetB: CustomerInputs(
-                  valueController: shoulderController,
-                  imageSource: 'assets/images/shoda.png',
-                  name: shoulder,
-                  textColor: CustomColors.c6,
-                ),
-              ),
-              //third row .
               ItemRows(
                 widgetA: CustomerInputs(
                   valueController: shoToWaistController,
@@ -115,6 +109,23 @@ class _LadiesTopState extends State<LadiesTop> {
                   textColor: CustomColors.c1,
                 ),
               ),
+
+              //third row .
+              ItemRows(
+                widgetA: CustomerInputs(
+                  valueController: waistController,
+                  imageSource: 'assets/images/waist.png',
+                  name: waist,
+                  textColor: CustomColors.c1,
+                ),
+                widgetB: CustomerInputs(
+                  valueController: hipController,
+                  imageSource: 'assets/images/hip.png',
+                  name: hip,
+                  textColor: CustomColors.c2,
+                ),
+              ),
+
               //fourth row .
               ItemRows(
                 widgetA: CustomerInputs(
@@ -126,7 +137,7 @@ class _LadiesTopState extends State<LadiesTop> {
                 widgetB: CustomerInputs(
                   valueController: aroundArmController,
                   imageSource: 'assets/images/arm.png',
-                  name: knee,
+                  name: aroundArm,
                   textColor: CustomColors.c6,
                 ),
               ),
@@ -140,18 +151,49 @@ class _LadiesTopState extends State<LadiesTop> {
                   ),
                   widgetB: Container()),
 
-              CustomDtPmt(),
+              CustomDtPmt(
+                dateTimeController: dtController,
+                paymentController: initialPaymentController,
+                serviceChargeController: serviceChargeController,
+              ),
 
               Center(
                 child: Container(
                   margin: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
                   child: FloatingActionButton.extended(
+                    label: Text(save),
                     onPressed: () {
                       if (_formKey.currentState!.validate()) {
-                        print('good to goo');
+                        var status = Dress().checkPaymentStatus(
+                            int.parse(serviceChargeController.text),
+                            int.parse(initialPaymentController.text));
+
+                        if (status.contains(error)) {
+                          ShowAction().showSnackbar(context);
+                        } else {
+                          _dressProvider.setLadiesTopData(
+                              nameController.text,
+                              phoneNumberController.text,
+                              shoulderController.text,
+                              bustController.text,
+                              nipToNipController.text,
+                              shoToNipController.text,
+                              waistController.text,
+                              hipController.text,
+                              shoToWaistController.text,
+                              aroundArmController.text,
+                              topLengthController.text,
+                              widget.month,
+                              int.parse(serviceChargeController.text),
+                              int.parse(initialPaymentController.text),
+                              dtController.text,
+                              status);
+
+                          _dressProvider.createNewDress(
+                              context, DressType.LADIES_TOP);
+                        }
                       }
                     },
-                    label: Text(save),
                   ),
                 ),
               )
