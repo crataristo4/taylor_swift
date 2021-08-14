80f77f12b45109d6c4b82b9843f7c7df901a39d9
Aristocrat <crataristo4@gmail.com>
2021-08-14 02:00:19 +0000

    added notification to on skirt saved

--- lib/ui/pages/ladies/ladies_skirt.dart
+++ lib/ui/pages/ladies/ladies_skirt.dart
@@ -1,15 +1,20 @@
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:taylor_swift/constants/constants.dart';
 import 'package:taylor_swift/constants/theme_data.dart';
 import 'package:taylor_swift/enum/enums.dart';
+import 'package:taylor_swift/helper/notification_info.dart';
 import 'package:taylor_swift/model/dress.dart';
 import 'package:taylor_swift/provider/dress_provider.dart';
+import 'package:taylor_swift/ui/home/home.dart';
 import 'package:taylor_swift/ui/widgets/actions.dart';
 import 'package:taylor_swift/ui/widgets/custom_dt_payment.dart';
 import 'package:taylor_swift/ui/widgets/custom_inputs.dart';
 import 'package:taylor_swift/ui/widgets/custom_name.dart';
 import 'package:taylor_swift/ui/widgets/item_rows.dart';
+
+import '../../../main.dart';
 
 class LadiesSkirt extends StatefulWidget {
   final month;
@@ -33,6 +38,13 @@ class _LadiesSkirtState extends State<LadiesSkirt> {
 
   final _formKey = GlobalKey<FormState>();
   DressProvider _dressProvider = DressProvider();
+  DateTime? _notificationTime;
+
+  @override
+  void initState() {
+    _notificationTime = DateTime.now();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -120,6 +132,7 @@ class _LadiesSkirtState extends State<LadiesSkirt> {
                   child: FloatingActionButton.extended(
                     onPressed: () {
                       if (_formKey.currentState!.validate()) {
+                        //_notificationTime = dtController.text as DateTime?;
                         var status = Dress().checkPaymentStatus(
                             int.parse(serviceChargeController.text),
                             int.parse(initialPaymentController.text));
@@ -142,6 +155,8 @@ class _LadiesSkirtState extends State<LadiesSkirt> {
 
                           _dressProvider.createNewDress(
                               context, DressType.LADIES_SKIRT);
+
+                          saveNotification();
                         }
                       }
                     },
@@ -154,5 +169,52 @@ class _LadiesSkirtState extends State<LadiesSkirt> {
         ),
       ),
     );
+  }
+
+  void scheduleNotification(
+      scheduledNotificationDateTime, NotificationInfo notificationInfo) async {
+    var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+      'notification',
+      'notification',
+      'Channel for Notification',
+      importance: Importance.high,
+      icon: 'launch_image', //todo -- change
+      sound: RawResourceAndroidNotificationSound('notif'),
+      largeIcon: DrawableResourceAndroidBitmap('launch_image'),
+    );
+
+    var iOSPlatformChannelSpecifics = IOSNotificationDetails(
+        sound: 'notif.mp3',
+        presentAlert: true,
+        presentBadge: true,
+        presentSound: true);
+    var platformChannelSpecifics = NotificationDetails(
+        android: androidPlatformChannelSpecifics,
+        iOS: iOSPlatformChannelSpecifics);
+
+    await flutterLocalNotificationsPlugin.schedule(
+      0,
+      notificationInfo.title,
+      "Is due!",
+      scheduledNotificationDateTime,
+      platformChannelSpecifics,
+      androidAllowWhileIdle: true,
+    );
+  }
+
+  void saveNotification() {
+    DateTime scheduleAlarmDateTime;
+    if (HomePage.notificationTime.isAfter(DateTime.now()))
+      scheduleAlarmDateTime = HomePage.notificationTime;
+    else
+      scheduleAlarmDateTime = HomePage.notificationTime.add(Duration(days: 1));
+
+    var notificationInfo = NotificationInfo(
+      notifDateTime: scheduleAlarmDateTime,
+      title: 'Time to submit ${nameController.text}\'s cloth',
+    );
+    // _notificationHelper.insertNotification(notificationInfo);
+    scheduleNotification(scheduleAlarmDateTime, notificationInfo);
+    print("saved to $scheduleAlarmDateTime");
   }
 }
