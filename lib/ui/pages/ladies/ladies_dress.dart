54892e835731929d1684af2722147ddac3bf1972
Aristocrat <crataristo4@gmail.com>
2021-08-14 17:43:31 +0000

    added logic

--- lib/ui/pages/ladies/ladies_dress.dart
+++ lib/ui/pages/ladies/ladies_dress.dart
@@ -2,7 +2,11 @@ import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:taylor_swift/constants/constants.dart';
 import 'package:taylor_swift/constants/theme_data.dart';
+import 'package:taylor_swift/enum/enums.dart';
+import 'package:taylor_swift/model/dress.dart';
+import 'package:taylor_swift/provider/dress_provider.dart';
 import 'package:taylor_swift/ui/home/home.dart';
+import 'package:taylor_swift/ui/widgets/actions.dart';
 import 'package:taylor_swift/ui/widgets/custom_dt_payment.dart';
 import 'package:taylor_swift/ui/widgets/custom_inputs.dart';
 import 'package:taylor_swift/ui/widgets/custom_name.dart';
@@ -29,15 +33,14 @@ class _LadiesDressState extends State<LadiesDress> {
   TextEditingController shoToWaistController = TextEditingController();
   TextEditingController kneeController = TextEditingController();
   TextEditingController dressLengthController = TextEditingController();
+  TextEditingController trouserLengthController = TextEditingController();
   TextEditingController sleeveLengthController = TextEditingController();
   TextEditingController aroundArmController = TextEditingController();
   TextEditingController dtController = TextEditingController();
-  TextEditingController paymentController = TextEditingController();
+  TextEditingController initialPaymentController = TextEditingController();
   TextEditingController serviceChargeController = TextEditingController();
-
+  DressProvider _dressProvider = DressProvider();
   final _formKey = GlobalKey<FormState>();
-
-  //todo - add notification schedule
 
   @override
   Widget build(BuildContext context) {
@@ -157,32 +160,71 @@ class _LadiesDressState extends State<LadiesDress> {
               ),
 
               ItemRows(
-                  widgetA: CustomerInputs(
-                    valueController: aroundArmController,
-                    imageSource: 'assets/images/arm.png',
-                    name: aroundArm,
-                    textColor: CustomColors.c6,
-                  ),
-                  widgetB: Container()),
+                widgetA: CustomerInputs(
+                  valueController: aroundArmController,
+                  imageSource: 'assets/images/arm.png',
+                  name: aroundArm,
+                  textColor: CustomColors.c6,
+                ),
+                widgetB: CustomerInputs(
+                  valueController: trouserLengthController,
+                  imageSource: 'assets/images/lt.png',
+                  name: trouserLength,
+                  textColor: CustomColors.c4,
+                ),
+              ),
 
               CustomDtPmt(
                 dateTimeController: dtController,
-                paymentController: paymentController,
+                paymentController: initialPaymentController,
+                serviceChargeController: serviceChargeController,
               ),
 
               Center(
                 child: Container(
                   margin: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
                   child: FloatingActionButton.extended(
-                    onPressed: () {
-                      if (_formKey.currentState!.validate()) {
-                        HomePage.nameControllerString = nameController.text;
-                        //call static method and schedule
-                        HomePage.saveNotification();
-                      }
-                    },
-                    label: Text(save),
-                  ),
+                      label: Text(save),
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          var status = Dress().checkPaymentStatus(
+                              int.parse(serviceChargeController.text),
+                              int.parse(initialPaymentController.text));
+
+                          if (status.contains(error)) {
+                            ShowAction().showSnackbar(context);
+                          } else {
+                            _dressProvider.setLadiesDressData(
+                              nameController.text,
+                              "${CustomNameAndNumber.cc}${phoneNumberController.text}",
+                              shoulderController.text,
+                              bustController.text,
+                              nipToNipController.text,
+                              shoToNipController.text,
+                              hipController.text,
+                              waistController.text,
+                              shoToWaistController.text,
+                              kneeController.text,
+                              dressLengthController.text,
+                              sleeveLengthController.text,
+                              aroundArmController.text,
+                              widget.month,
+                              int.parse(serviceChargeController.text),
+                              int.parse(initialPaymentController.text),
+                              dtController.text,
+                              trouserLengthController.text,
+                            );
+
+                            HomePage.nameControllerString = nameController.text;
+
+                            _dressProvider.createNewDress(
+                                context, DressType.LADIES_DRESS);
+
+                            //call static method and schedule
+                            HomePage.saveNotification();
+                          }
+                        }
+                      }),
                 ),
               )
             ],
